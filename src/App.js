54f3b8b55a2e93f7dff913e422788d8fc533fda1
Emma Andersson <Emma@Emmas-Air-2.lan>
2021-06-04 15:32:46 +0200

    added enpoint, parameters and url for fetching api, and created function for checking wheater

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Fetch from './components/Fetch';
+import WeatherList from './components/WeatherList';
+
 
 const App = () => {
-  const { data, error, isLoading, setUrl } = UseFetch();
+  const { data, error, isLoading, setUrl } = Fetch();
+  const [city, setCity] = useState('');
+
+  useEffect(() => {
+    console.log(city)
+  }, [city]);
+
+  const ENDPOINT = 'https://api.openweathermap.org/';
+  const PART = 'hourly,minutely,current,alerts';
+  const KEY = '3cfb96dd5ec2730ca9ea11397535e2b7';
+
+  const STHLM = ['59.32', '18.06'];
+  const BERLIN = ['52.52', '13.40'];
+  const OSLO = ['59.91', '10.75'];
+  let url = "";
+
+  function createUrl(event) {
+    setCity(event.target.value);
+    console.log(city + 34)
+    let lat = '';
+    let lon = '';
+
+    if (city === 'sthlm') {
+      lat = STHLM[0];
+      lon = STHLM[1];
+    } else if (city === 'berlin') {
+      lat = BERLIN[0];
+      lon = BERLIN[1];
+    } else {
+      lat = OSLO[0];
+      lon = OSLO[1];
+    }
+    url = `${ENDPOINT}data/2.5/onecall?lat=` + lat + `&lon=` + lon + `&exclude=${PART}&units=metric&appid=${KEY}`;
+    setUrl(url);
+  };
 
   const fetchContent = () => {
     if (error) return <p>Error when fetching: {error}</p>
@@ -12,6 +50,14 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Choose city to check weather</h1>
+      <select className="select" value={city} onChange={event => createUrl(event)}>
+        <option value='cities'> -- cities -- </option>
+        <option value='sthlm'> STOCKHOLM </option>
+        <option value='berlin'> BERLIN </option>
+        <option value='oslo'> OSLO </option>
+      </select>
+
       {fetchContent()}
     </div>
   );
