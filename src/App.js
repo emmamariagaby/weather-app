1f2f23167e4862cd9ab8430a0e1354728a554c2f
Emma Andersson <Emma@Emmas-Air-2.lan>
2021-06-04 15:16:15 +0200

    created fetch component and related function for handling data, endpoint and errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
-function App() {
+const App = () => {
+  const { data, error, isLoading, setUrl } = UseFetch();
+
+  const fetchContent = () => {
+    if (error) return <p>Error when fetching: {error}</p>
+    if (!data && isLoading) return <p>LOADING...</p>
+    if (!data) return null;
+    return <WeatherList weathers={data.daily} />
+  };
+
   return (
     <div className="App">
-
+      {fetchContent()}
     </div>
   );
 }
+
 
 export default App;
