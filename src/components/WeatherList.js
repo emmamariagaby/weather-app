6a97d52ada1091ad2a8f4740a68a7ad326883ed6
Emma Andersson <Emma@Emmas-Air-2.lan>
2021-06-05 22:12:08 +0200

    removed the last element from weatherlist and added styled component

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import WeatherCard from './WeatherCard';
+import styled from 'styled-components';
+
+
+const CardBodyContainer = styled.div`
+margin: 1rem;
+display: flex;
+flex-flow: row wrap;
+justify-content: center;
+background-color: orange;
+border: solid 0.5rem darkorange;
+`;
 
 const WeatherList = ({ weathers }) => {
+    weathers.pop();
     return (
-        <>
+        <CardBodyContainer>
             {weathers.map(({ dt, temp, weather }) => (
                 <div key={dt}>
                     <WeatherCard temp_max={temp.max} temp_min={temp.min} dt={dt * 1000} main={weather[0].main} icon={weather[0].icon} />
                 </div>
             ))}
-        </>
+        </CardBodyContainer>
     )
 }
 
