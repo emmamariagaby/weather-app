4bcccb7c64b2010073416ebc84adf5294ba3d8ef
Emma Andersson <Emma@Emmas-Air-2.lan>
2021-06-05 22:14:07 +0200

    added styled component

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CardBody = styled.div`
+padding: 1rem;
+margin: 1rem;
+text-align: center;
+height: 280px;
+width: 240px;
+border-radius: 20px;
+color: black;
+background-color: lightgrey;
+`;
 
 const WeatherCard = ({ dt, temp_min, temp_max, main, icon }) => {
     const date = new Date(dt);
     return (
-        <>
+        <CardBody>
             <img
                 variant="top"
                 src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="img"
@@ -14,7 +26,7 @@ const WeatherCard = ({ dt, temp_min, temp_max, main, icon }) => {
             </p>
             <p>Min: {temp_min}</p>
             <p>Max: {temp_max}</p>
-        </>
+        </CardBody>
     );
 };
 
